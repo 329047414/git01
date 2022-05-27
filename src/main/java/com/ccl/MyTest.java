0451d97f27879bd98d6251a4bf6f99e5750fee71
13813367076 <13813367076@qq.com>
2022-05-27 16:08:14 +0800

    first commit

---
+++ src/main/java/com/ccl/MyTest.java
@@ -0,0 +1,11 @@
+package com.ccl;
+
+/**
+ * @author 329047414
+ * @create 2022-05-27-16:02
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
