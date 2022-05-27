44473d5b6a0a6c39e745c9c27c76c8267e5410ea
13813367076 <13813367076@qq.com>
2022-05-27 16:13:55 +0800

    third commit

--- src/main/java/com/ccl/MyTest.java
+++ src/main/java/com/ccl/MyTest.java
@@ -8,5 +8,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("hello,git");
+        System.out.println("hello,java");
     }
 }
